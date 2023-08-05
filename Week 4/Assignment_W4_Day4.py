90dc259066b1d464d45ae756ac31c10aa1211dbb
diptanu <diptanudn777@gmail.com>
2023-08-05 18:51:39 +0530

    Assignment W4 y4 Q7 update

--- Week 4/Assignment_W4_Day4.py
+++ Week 4/Assignment_W4_Day4.py
@@ -57,20 +57,28 @@
 
 # #7. Write code that takes a list of numbers and an integer 'n' as input. Print the index of the first occurrence of 'n' in the list, or print "Not found" if 'n' is not present.
 
-# ???
+a = [54, 2, 1, 43, 676, 45, 45, 45, 21, 787, 45]
+num = int(input("Enter a number = "))
+if a.count(num) > 0:
+    for i in range(0, len(a)):
+        if a[i] == num:
+            print(f"{num} is at position = {i}")
+            break
+else:
+    print("Not found")
 
 # #8. Write code that takes a list of numbers as input and calculates the sum of elements at even indices and the sum of elements at odd indices separately. Print both sums.
-n = int(input("How many numbers do you want to enter? "))
-numbers = []
-for i in range(n):
-    num = int(input(f"Enter number {i+1}: "))
-    numbers.append(num)
-even_sum = 0
-odd_sum = 0
-for i in range(len(numbers)):
-    if i % 2 == 0:
-        even_sum += numbers[i]
-    else:
-        odd_sum += numbers[i]
-print(f"The sum of elements at even indices is {even_sum}")
-print(f"The sum of elements at odd indices is {odd_sum}")
+# n = int(input("How many numbers do you want to enter? "))
+# numbers = []
+# for i in range(n):
+#     num = int(input(f"Enter number {i+1}: "))
+#     numbers.append(num)
+# even_sum = 0
+# odd_sum = 0
+# for i in range(len(numbers)):
+#     if i % 2 == 0:
+#         even_sum += numbers[i]
+#     else:
+#         odd_sum += numbers[i]
+# print(f"The sum of elements at even indices is {even_sum}")
+# print(f"The sum of elements at odd indices is {odd_sum}")
